e22865e8781b217ae31ee3db6961a10d711db9ca
Ivan Wang <icw@cmu.edu>
2016-02-06 02:25:09 -0500

    stalk friends data from fb

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+  $.ajax({
+    url: '/friend',
+    type: 'GET',
+    success: function(json) {
+      console.log(json);
+    },
+    error: function(xhr, status, error) {
+      console.log("error getting friend!", error);
+    }
+  });
+
 });
