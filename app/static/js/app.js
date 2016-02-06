c80bd48d96b474df0eea8ea48513da04f8cd63a4
ewang <ewang@ewangs-MBP.wv.cc.cmu.edu>
2016-02-06 02:25:28 -0500

    some app.js changes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+    // expected format: { them: <character speech>, you: [<response option 1>, <opt 2>, ...]}
+    $.get('ajax/babble', function (data) {
+    $('#mainDiv').append('<p>' + data['them'] + '</p>')
+    $('#mainDiv').append('<ul>')
+    // TODO: replace existing list
+    for (var i = 0; i < data['you'].length; i++) {
+        $('#mainDiv').append('<li><a href="#" onclick="selectChoice()>' 
+            + data['you'][i] + '</a></li>')
+    }
 
 });
