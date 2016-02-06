8eb2e5bdd663615a85a413ebaaaa1f78a18aeb06
ewang <ewang@ewangs-MBP.wv.cc.cmu.edu>
2016-02-06 17:13:31 -0500

    response probabilities

--- app/static/js/dating.js
+++ app/static/js/dating.js
@@ -1,19 +1,21 @@
 (function() {
   var firstName = friendName.substring(0, friendName.indexOf(' '));
   var userFirstName = userName.substring(0, userName.indexOf(' '));
+  var gameOver = false;
 
+  // Response and probability of increasing affection.
   var responses = [
-    'Will you marry me?', 
-    'Really?', 
-    'Sorry', 
-    firstName+'-Senpai!', 
-    'I’ll beat him up for you', 
-    'Er…', 
-    'Happy birthday', 
-    'But I\'m already taken', 
-    'What was that?', 
-    'Of course. I understand.', 
-    'Wait!'
+    ['Will you marry me?', 0.2],
+    ['Really?', 0.5],
+    ['Sorry', 0.9],
+    [firstName+'-Senpai!', 1.0],
+    ['I’ll beat him up for you', 1.0],
+    ['Er…', 0.5],
+    ['Happy birthday', 0.8],
+    ['But I\'m already taken', 0.2],
+    ['What was that?', 0.3],
+    ['Of course. I understand.', 0.8],
+    ['Wait!', 0.7]
   ]
 
   var intro = 'It\'s an unusually nice day at Carnegie Mellon University. You\'ve just finished your classes and are ready to head home when you hear the quiet sound of footsteps.';
@@ -27,6 +29,10 @@
   var progress = 0;
   var replies;
   function playScene(num) {
+    if (gameOver) {
+      num = 10;
+    }
+
     switch (num) {
       case 0:
         hideName();
@@ -42,6 +48,7 @@
 
       case 5:
         $.get('/birthday', function (data) {
+          console.log(data['birthday']);
           if (data['birthday']) {
             loadDialogue(dia2, ['Of course. It\'s ' + birthday]);
           } else {
@@ -63,9 +70,9 @@
     }
   }
 
-  function selectChoice() {
+  function selectChoice(prob) {
     console.log('clicked');
-    updateAffection();
+    updateAffection(prob);
     clearDialogue();
     progress++;
     playScene(progress);
@@ -90,14 +97,20 @@
   }
 
   var intervalId;
-  function updateAffection() {
-    var incr = Math.random() * 0.25;
-    if (affection > incr && Math.random() < 0.7) {
-      incr = incr * -1;
-    } else if (affection + incr >= 1.0) {
-      // you win
+  function updateAffection(prob) {
+    if (progress == 0) {
+      return;
     }
-    affection += incr;
+    var incr = Math.random() * 0.5;
+    if (affection > incr && Math.random() > prob) {
+      incr = incr * -1;
+      affection += incr;
+    } else if (affection + incr >= 1.0) {
+      gameOver = true;
+      affection = 1.0;
+    } else {
+      affection += incr;
+    }
     var width = $('#affection').width();
     var newWidth = Math.floor(width * affection);
     $('#bar').animate({
@@ -119,7 +132,7 @@
     for (var i = 4; i < responses.length; i++) {
       var rand = Math.floor(Math.random() * i);
       if (rand < 4) {
-        curr[rand] = responses[i];
+        curr[rand] = responses[i][0];
       }
     }
     return curr;
@@ -130,8 +143,8 @@
     $.each(replies, function(i) {
       var li = $('<li/>').appendTo(responseList);
       var ahref = $('<a/>')
-        .on('click', selectChoice)
-        .text(replies[i])
+        .on('click', { prob: replies[i][1] }, selectChoice)
+        .text(replies[0][i])
         .appendTo(li);
       });
     $('#dialogue-box').append(responseList);
